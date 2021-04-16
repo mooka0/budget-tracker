08f2bca2cf01b173472c1bd3e7ec1c75f9b806f3
mooka0 <angelankao@gmail.com>
2021-04-15 17:30:57 -0700

    delete node modules

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 const app = express();
 
@@ -15,11 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
 
 // routes
 app.use(require("./routes/api.js"));
