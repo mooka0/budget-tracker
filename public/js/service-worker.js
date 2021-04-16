3afd5d3c3aede010d95343abce8c5ddb46b843ca
mooka0 <angelankao@gmail.com>
2021-04-15 22:53:59 -0700

    fixed 2

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,5 +1,5 @@
 // global constants 
-const APP_PREFIX = 'FoodFest-';     
+const APP_PREFIX = 'Budget-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
@@ -57,16 +57,13 @@ self.addEventListener('fetch', function (e) {
     caches.match(e.request).then(function (request) {
       if (request) { // if cache is available, respond with cache
         console.log('responding with cache : ' + e.request.url)
-      //   console.log(e.request.url)
         return request
       } else {       // if there are no cache, try fetching request
         console.log('file is not cached, fetching : ' + e.request.url)
-      //   console.log(e.request.url)
         return fetch(e.request)
       }
 
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
+  
     })
   )
 });
